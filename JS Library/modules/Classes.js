851f9a145a81dbc68ce2b83a8aa163a43f95b238
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2014-05-27 19:53:53 -0400

    Updated Classes.js
    Added in "El" namespace.

--- JS Library/modules/Classes.js
+++ JS Library/modules/Classes.js
@@ -1,4 +1,4 @@
-/* addClass / removeClass / hasClass / toggleClass
+/* El.addClass / El.removeClass / El.hasClass / El.toggleClass
  *
  * Adds/removes a specified class; detects whether or not a specified element has a specified class; toggles a specified class
  *
@@ -11,7 +11,7 @@
 	
 	// Detect is a given element has a particular class
 	
-	hasClass = function(element,cls){
+	El.hasClass = function(element,cls){
 		if (testParam(element)){
 			return element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
 		} else {
@@ -21,9 +21,9 @@
 	
 	// Add the specified className to the element if it doesn't already contain that class
 	
-	addClass = function(element,cls){
+	El.addClass = function(element,cls){
 		if (testParam(element)){
-			if (!hasClass(element,cls)){
+			if (!El.hasClass(element,cls)){
 				if (testParam(cls)){
 					if (element.className == ""){
 						element.className += cls;
@@ -41,9 +41,9 @@
 	
 	// Remove the specified className from the element if it contains that class
 	
-	removeClass = function(element,cls) {
+	El.removeClass = function(element,cls) {
 		if (testParam(element)){
-			if (hasClass(element,cls)){
+			if (El.hasClass(element,cls)){
 				if (testParam(cls)){
 					var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
 					element.className = element.className.replace(reg,"");
@@ -58,12 +58,12 @@
 	
 	// Toggles the specified className
 	
-	toggleClass = function(element,cls){
+	El.toggleClass = function(element,cls){
 		if (testParam(element)){
-			if (!hasClass(element,cls)){
-				addClass(element,cls);
-			} else if (hasClass(element,cls)){
-				removeClass(element,cls);
+			if (!El.hasClass(element,cls)){
+				El.addClass(element,cls);
+			} else if (El.hasClass(element,cls)){
+				El.removeClass(element,cls);
 			}
 		} else {
 			throw new Error("Cannot toggle class for undefined element.");
