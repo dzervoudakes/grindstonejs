573121c3f42841f0a69f95eafcf3be84b381862a
DRZervoudakes <dan.zervoudakes@gmail.com>
2014-08-12 20:42:33 -0400

    Finalized array support for existing modules except Fade.js

--- JS Library/modules/Classes.js
+++ JS Library/modules/Classes.js
@@ -11,61 +11,133 @@
 	// Detect if a given element has a particular class
 	
 	Grindstone.prototype.hasClass = function(cls){
-		if (testParam(cls)){
-			var element = this.init;
-			return element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+		var results = this.init;
+		if (results.length > 1){
+			for (var i = 0; i < results.length; i++){
+				var element = results[i];
+				if (testParam(cls)){
+					return element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+				} else {
+					throw new Error("Cannot determine if the element has undefined class.");
+				}
+			};
 		} else {
-			throw new Error("Cannot determine if the element has undefined class.");
+			var element = results;
+			if (testParam(cls)){
+				return element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+			} else {
+				throw new Error("Cannot determine if the element has undefined class.");
+			}
 		}
-	}
+	};
 	
 	// Add the specified class to the element if it doesn't already contain that class
 	
 	Grindstone.prototype.addClass = function(cls){
-		var element = this.init;
-		if (!this.hasClass(cls)){
-			if (testParam(cls)){
-				if (element.className == ""){
-					element.className += cls;
-					return this;
-				} else {
-					element.className += " " + cls;
-					return this;
+		var results = this.init;
+		if (results.length > 1){
+			for (var i = 0; i < results.length; i++){
+				var element = results[i];
+				if (!element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+					if (testParam(cls)){
+						if (element.className == ""){
+							element.className += cls;
+						} else {
+							element.className += " " + cls;
+						}
+					} else {
+						throw new Error("Class to add is undefined.");
+					}
 				}
-			} else {
-				throw new Error("Class to add is undefined.");
+			};
+			return this;
+		} else {
+			var element = results;
+			if (!element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+				if (testParam(cls)){
+					if (element.className == ""){
+						element.className += cls;
+					} else {
+						element.className += " " + cls;
+					}
+				} else {
+					throw new Error("Class to add is undefined.");
+				}
 			}
+			return this;
 		}
-	}
+	};
 	
 	// Remove the specified class from the element if it contains that class
 	
 	Grindstone.prototype.removeClass = function(cls){
-		var element = this.init;
-		if (this.hasClass(cls)){
-			if (testParam(cls)){
-				var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-				element.className = element.className.replace(reg,"");
-				return this;
-			} else {
-				throw new Error("Class to remove is undefined.");
+		var results = this.init;
+		if (results.length > 1){
+			for (var i = 0; i < results.length; i++){
+				var element = results[i];
+				if (element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+					if (testParam(cls)){
+						var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+						element.className = element.className.replace(reg,"");
+					} else {
+						throw new Error("Class to remove is undefined.");
+					}
+				}
+			};
+			return this;
+		} else {
+			var element = results;
+			if (element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+				if (testParam(cls)){
+					var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+					element.className = element.className.replace(reg,"");
+				} else {
+					throw new Error("Class to remove is undefined.");
+				}
 			}
+			return this;
 		}
-	}
+	};
 	
 	// Toggle the specified class
 	
 	Grindstone.prototype.toggleClass = function(cls){
-		var element = this.init;
-		if (testParam(cls)){
-			if (!this.hasClass(cls)){
-				this.addClass(cls);
-				return this;
-			} else if (this.hasClass(cls)){
-				this.removeClass(cls);
-				return this;
-			}
+		var results = this.init;
+		if (results.length > 1){
+			for (var i = 0; i < results.length; i++){
+				var element = results[i];
+				if (testParam(cls)){
+					if (!element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+						if (element.className == ""){
+							element.className += cls;
+						} else {
+							element.className += " " + cls;
+						}
+					} else if (element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+						var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+						element.className = element.className.replace(reg,"");
+					}
+				} else {
+					throw new Error("Class to toggle is undefined.");
+				}
+			};
+			return this;
 		} else {
-			throw new Error("Class to toggle is undefined.");
+			var element = results;
+			if (testParam(cls)){
+				if (!element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+					if (element.className == ""){
+						element.className += cls;
+					} else {
+						element.className += " " + cls;
+					}
+				} else if (element.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))){
+					var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+					element.className = element.className.replace(reg,"");
+				}
+			} else {
+				throw new Error("Class to toggle is undefined.");
+			}
+			return this;
 		}
 	};
