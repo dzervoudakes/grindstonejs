9c6754a4c1d2b1671660fd86a0fc8c58aeaf8b3e
DRZervoudakes <dan.zervoudakes@gmail.com>
2014-10-11 19:52:56 -0400

    Namespaced 'xmlhttp' variable for Ajax module... This represents the last of the global variables in need of namespacing.

--- JS Library/modules/Ajax.js
+++ JS Library/modules/Ajax.js
@@ -9,20 +9,20 @@
  * -callback (function to invoke if successful)
  */
 	
-	var xmlhttp;
+	$.xmlhttp;
 	
 	$.ajax = function(method,url,async,callback){
 		if (method && url && async && callback){ // All parameters must be defined
-			xmlhttp = new XMLHttpRequest(); // Modern browsers
-			xmlhttp.onreadystatechange = function(){
-				if (xmlhttp.readyState == 4 && (xmlhttp.status == 200 || xmlhttp.status == 304)){
+			$.xmlhttp = new XMLHttpRequest();
+			$.xmlhttp.onreadystatechange = function(){
+				if ($.xmlhttp.readyState == 4 && ($.xmlhttp.status == 200 || $.xmlhttp.status == 304)){
 					callback();
 				}
 			};
-			xmlhttp.open(method,url,async);
-			xmlhttp.send();
+			$.xmlhttp.open(method,url,async);
+			$.xmlhttp.send();
 		} else {
 			throw new Error("Please define 'GET'/'POST', the URL, Async true/false, and the function to be ran if successful.");
 		}
-		return xmlhttp;
+		return $.xmlhttp;
 	};
