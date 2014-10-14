f4ed216e91fda3fd726c87016d4551021ad0d4b9
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2014-10-13 21:25:59 -0400

    Extended Mouseable module...
    ...now users have the ability to program their own "hover" and "active" state classes; if no classes are defined, the hover class defaults to "over" and the active class defaults to "down".

--- JS Library/modules/Mouseable.js
+++ JS Library/modules/Mouseable.js
@@ -1,26 +1,41 @@
 /* mouseable()
  *
- * Dynamically adds class "over" to elements as a hover state
- * Dynamically adds class "down" to elements as an active state
+ * Dynamically adds class "over" to elements as a hover state (default)
+ * Dynamically adds class "down" to elements as an active state (default)
  * Removes the need for applicable CSS pseudo-states
  * Handles both standard mouse events and touch events
+ * 
+ * Parameter:
+ * -classes = an object users may program to create their own "hover" and "active" classes (accepts properties "hoverClass" and "activeClass")
  */
 	
-	$.gs.mouseable = function(){
+	$.gs.mouseable = function(classes){
+		var hoverClass,
+			activeClass;
+		if (classes){
+			if (typeof classes === "object"){
+				hoverClass = (classes.hasOwnProperty("hoverClass")) ? classes["hoverClass"] : "over";
+				activeClass = (classes.hasOwnProperty("activeClass")) ? classes["activeClass"] : "down";
+			} else {
+				throw new Error("Classes parameter for mouseable() must be an object with properties 'hoverClass' and/or 'activeClass'.");
+			}
+		} else {
+			hoverClass = "over";
+			activeClass = "down";
+		}
 		$.forEach(this.init,function(){
 			$(this).evt("mouseover touchstart",function(e){
-				$(this).addClass("over");
+				$(this).addClass(hoverClass);
 				e.preventDefault();
 			}).evt("mouseleave touchend",function(e){
-				$(this).removeClass("over down").removeClass("over");
+				$(this).removeClass(hoverClass + " " + activeClass).removeClass(hoverClass);
 				e.preventDefault();
 			}).evt("mousedown touchstart",function(e){
-				$(this).addClass("down");
+				$(this).addClass(activeClass);
 				e.preventDefault();
 			}).evt("mouseup touchend",function(e){
-				$(this).removeClass("down");
+				$(this).removeClass(activeClass);
 				e.preventDefault();
 			});
 		});
-		return this;
 	};
