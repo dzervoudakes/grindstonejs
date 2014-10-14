23f3ac2912bea9298a044d2b61ae1ef2adaa0154
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2014-10-13 21:50:48 -0400

    Extended Events Module...
    ...I figure if users can add events to elements, they should be able to drop events as well...

--- JS Library/modules/Events.js
+++ JS Library/modules/Events.js
@@ -1,11 +1,13 @@
-/* evt()
+/* evt() / dropEvt()
  * 
- * Custom event listener method
+ * Adding and removing event listeners
  *
  * Parameters:
  * -action (the event to handle: only accepts one event per function call)
  * -callback (the event handler)
  */
+	
+	// Assign the eventListener
 	
 	$.gs.evt = function(action,callback){
 		$.forEach(this.init,function(){
@@ -19,8 +21,28 @@
 					throw new Error("Type of event action must be a string.");
 				}
 			} else {
-				throw new Error("Event to handle is undefined.")
+				throw new Error("Event to handle is undefined.");
 			}
 		});
 		return this;
  	};
+	
+	// Drop the eventListener
+	
+	$.gs.dropEvt = function(action,callback){
+		$.forEach(this.init,function(){
+			if (action){
+				if (typeof action === "string"){
+					var events = action.split(" ");
+					for (var j = 0; j < events.length; j++){
+						this.removeEventListener(events[j],callback,false);
+					};
+				} else {
+					throw new Error("Type of event action must be a string.");
+				}
+			} else {
+				throw new Error("Event handler to drop is undefined.");
+			}
+		});
+		return this;
+	};
