215264e66608f3e0d44c69dc83d8bd7795b58b91
DRZervoudakes <dan.zervoudakes@gmail.com>
2014-10-14 22:44:27 -0400

    Created 'each()' method.

---
+++ JS Library/modules/Each.js
@@ -0,0 +1,15 @@
+/* each()
+ * 
+ * Iterates through each item in the set and executes the callback
+ *
+ * Parameter:
+ * -callback (function called once for each item in the set)
+ */
+	
+	$.gs.each = function(callback){
+		var results = this.init;
+		for (var j = 0; j < results.length; j++){
+			callback.call(results[j]);
+		};
+		return this;
+ 	};
