e2ce63c480f775a4d4a1478dc767cf08afcefc6e
DRZervoudakes <dan.zervoudakes@gmail.com>
2014-10-14 21:56:22 -0400

    Created 'doubleTap/doubleClick' method and tested.

---
+++ JS Library/modules/DoubleTap.js
@@ -0,0 +1,29 @@
+/* doubleTap() / doubleClick()
+ * 
+ * Custom double-tapping and double-clicking methods
+ *
+ * Parameter:
+ * -callback (triggered if the double-click/tap event is completed in time)
+ */
+	
+	$.gs.doubleTap = function(callback){
+		$.forEach(this.init,function(){
+			var active = false;
+			if(callback){
+				$(this).evt("click touchstart",function(e){
+					if (active){
+						callback();
+						active = false;
+					}
+					active = true;
+					setTimeout(function(){
+						active = false;
+					},350);
+					e.preventDefault();
+				});
+			} else {
+				throw new Error("Double-tap/double-click callback is undefined.");
+			}
+		});
+		return this;
+ 	};
