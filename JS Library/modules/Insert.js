5d2247a8ff356d265f2912df4efe93bc072bd681
DRZervoudakes <dan.zervoudakes@gmail.com>
2014-10-20 16:42:40 -0400

    Quick edits to 'Insert' module.

--- JS Library/modules/Insert.js
+++ JS Library/modules/Insert.js
@@ -1,4 +1,4 @@
-/* insert()
+/* before() / after()
  *
  * Inserts new content either before or after the target element
  *
@@ -7,19 +7,13 @@
  * -location (the content will be inserted either before or after the target element within the DOM hierarchy)
  */
 	
-	$.gs.insertAfter = function(content){
+	$.gs.before = function(content){
 		$.forEach(this.init,function(){
 			if (content){
 				if (typeof content === "string"){
-					this.insertAdjacentHTML("afterend",content);
+					this.insertAdjacentHTML("beforebegin",content);
 				} else {
-					if (content.length > 1){
-						for (var j = 0; j < content.length; j++){
-							this.parentNode.insertBefore(content[j],this.nextSibling);
-						};
-					} else {
-						this.parentNode.insertBefore(content,this.nextSibling);
-					}
+					this.parentNode.insertBefore(content,this);
 				}
 			} else {
 				throw new Error("Cannot insert null after the specified element.");
@@ -28,19 +22,13 @@
 		return this;
 	};
 	
-	$.gs.insertBefore = function(content){
+	$.gs.after = function(content){
 		$.forEach(this.init,function(){
 			if (content){
 				if (typeof content === "string"){
-					this.insertAdjacentHTML("beforebegin",content);
+					this.insertAdjacentHTML("afterend",content);
 				} else {
-					if (content.length > 1){
-						for (var j = 0; j < content.length; j++){
-							this.parentNode.insertBefore(content[j],this);
-						};
-					} else {
-						this.parentNode.insertBefore(content,this);
-					}
+					this.parentNode.insertBefore(content,this.nextSibling);
 				}
 			} else {
 				throw new Error("Cannot insert null after the specified element.");
