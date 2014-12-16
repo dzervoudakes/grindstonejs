4e9ea2c59aab9668642bfc95f1aca76a2887505a
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2014-12-15 20:36:54 -0500

    Fixed an error in 'offset()'.

--- JS Library/modules/Offset.js
+++ JS Library/modules/Offset.js
@@ -1,7 +1,7 @@
 /**
  * offset()
  *
- * Returns the left/top offset value of the specified selector as an integer
+ * Returns the left/top offset value of the specified selector relative to the document (as an integer)
  * This will only apply to the first match in the set and includes margins
  *
  * Parameter:
@@ -13,10 +13,23 @@
 			if (position !== "left" && position !== "top"){
 				throw new Error("Offset position must be either 'left' or 'top'.");
 			} else {
+				var elem = this.init[0];
 				if (position === "left"){
-					return this.init[0].offsetLeft;
-				} else {
-					return this.init[0].offsetTop;
+					var offsetLeft = 0;
+				    do {
+				        if (!isNaN(elem.offsetLeft)){
+				          offsetLeft += elem.offsetLeft;
+				        }
+				    } while (elem = elem.offsetParent);
+				    return offsetLeft;
+				} else if (position === "top") {
+					var offsetTop = 0;
+				    do {
+				        if (!isNaN(elem.offsetTop)){
+				          offsetTop += elem.offsetTop;
+				        }
+				    } while (elem = elem.offsetParent);
+				    return offsetTop;
 				}
 			}
 		} else {
