cb6e4f07c69a77d10869304c0889fb116e0c08ec
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-01-26 17:37:18 -0500

    Added 'scroll()' module.

---
+++ JS Library/modules/Scroll.js
@@ -0,0 +1,19 @@
+/**
+ * scroll()
+ * 
+ * Captures the native "onscroll" event and executes a function each time the event triggers
+ * 
+ * Parameter:
+ * -callback
+ */
+	
+	$.fn.scroll = function(_callback){
+		$.forEach(this.init,function(){
+			$(this).evt("scroll",function(){
+				if(_callback){
+					_callback();
+				}
+			});
+		});
+		return this;
+ 	};
