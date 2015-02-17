85c831fe5070f26cea50b90d41eb220fcc017d05
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-02-16 22:45:39 -0500

    Updated CSS module functionality.

--- bin/grindstone-v1.1.4.js
+++ bin/grindstone-v1.1.4.js
@@ -355,32 +355,45 @@
 /**
  * css()
  *
- * Adjusts the CSS styles of a selected element if an object is passed as the argument
- * Returns the specified CSS value if a string is passed as the argument
+ * Adjusts the CSS styles of a selected element if an object is passed as the argument or if both the styles and value arguments are passed as strings
+ * Returns the specified CSS value if a string is passed as the styles argument and the value argument is null
  *
  * Parameter:
  * -styles (can be programmed as an object or a string)
+ * -value (optional; the new value of the styles argument, assuming styles is a string with a valid CSS property)
  */
 	
-	$.fn.css = function(_styles){
+	$.fn.css = function(_styles, _value){
 		
 		var returnedStyle, i, toReturn;
 		
 		this.init(function(){
+			
 			if (_styles){
+				
 				if (typeof _styles === "object"){
+					
 					for (i in _styles){
 						this.style[i] = _styles[i];
 					}
-				} else {
+					
+				} else if (_styles && typeof _styles === "string" && !_value) {
+					
 					returnedStyle = this.style[_styles];
+					
+				} else if (_styles && typeof _styles === "string" && _value && typeof _value === "string"){
+					
+					this.style[_styles] = _value;
+					
 				}
+				
 			} else {
 				throw new Error("CSS properties are undefined.");
 			}
+			
 		});
 		
-		toReturn = (typeof _styles === "object") ? this : returnedStyle;
+		toReturn = (typeof _styles === "object" || typeof _styles === "string" && _value) ? this : returnedStyle;
 		return toReturn;
 	};
 
