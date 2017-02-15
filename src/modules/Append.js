0c8e2a8a467d2cca0aea483ea4b6926ee67db74a
Chris Miller <chris@cmiller.me>
2017-02-15 21:05:01 +0000

    Append clone of element if appending to more than one

--- src/modules/Append.js
+++ src/modules/Append.js
@@ -6,7 +6,9 @@
 
 	$.fn.append = function(element) {
 		var isHTML = typeof element === 'string' && element.match(/(<).+(>)/);
+		var i = -1, len = this.length;
 		this.each(function() {
+			i++;
 			if (typeof element === 'string') {
 				if (isHTML) {
 					this.innerHTML += element;
@@ -15,7 +17,13 @@
 					this.appendChild(textNode);
 				}
 			} else {
-				this.appendChild(element);
+				if (i == len - 1) {
+					// Append element itself if last.
+					this.appendChild(element);
+				} else {
+					// Append a clone for all but the last.
+					this.appendChild(element.cloneNode(true));
+				}
 			}
 		});
 		return this;
