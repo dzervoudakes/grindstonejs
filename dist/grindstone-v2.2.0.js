e59034a0f7f35293ce10d53e520c1ba8b798f6e4
Chris Miller <chris@cmiller.me>
2017-02-19 09:03:04 +0000

    Undo change to remove
    It should only remove direct descendants. Choosing to go with the
    original version until the changes are correct.

--- dist/grindstone-v2.2.0.js
+++ dist/grindstone-v2.2.0.js
@@ -873,13 +873,20 @@
 
 /**
  * Remove elements from the DOM
- * @param {object} target - target element(s) or selector, optional: if omitted, the element(s) invoking this method will be removed
+ * @param {object} target - target element(s), optional: if omitted, the element(s) invoking this method will be removed
  * @returns {object} current instance of Grindstone
  */
 
 	$.fn.remove = function(target) {
 		if (target) {
-			$(target, this).remove();
+			var elems = d.querySelectorAll(target);
+			elems = Array.prototype.slice.call(elems);
+			this.each(function() {
+				var self = this;
+				elems.forEach(function(el) {
+					self.removeChild(el);
+				});
+			});
 		} else {
 			this.each(function() {
 				this.parentNode.removeChild(this);
