e31f8ba4dd2dcebb28f1818f1d531ae898cc6c76
Chris Miller <chris@cmiller.me>
2017-02-19 06:53:09 +0000

    Simplify and ensure target selector is descendant of the element

--- dist/grindstone-v2.2.0.js
+++ dist/grindstone-v2.2.0.js
@@ -890,20 +890,13 @@
 
 /**
  * Remove elements from the DOM
- * @param {object} target - target element(s), optional: if omitted, the element(s) invoking this method will be removed
+ * @param {object} target - target element(s) or selector, optional: if omitted, the element(s) invoking this method will be removed
  * @returns {object} current instance of Grindstone
  */
 
 	$.fn.remove = function(target) {
 		if (target) {
-			var elems = d.querySelectorAll(target);
-			elems = Array.prototype.slice.call(elems);
-			this.each(function() {
-				var self = this;
-				elems.forEach(function(el) {
-					self.removeChild(el);
-				});
-			});
+			$(target, this).remove();
 		} else {
 			this.each(function() {
 				this.parentNode.removeChild(this);
