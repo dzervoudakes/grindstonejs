467d6f0dd3b21093ccfcfe2a1c8ad843c54c8079
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2019-07-21 19:46:02 -0600

    Fool proofing

--- src/lib/manipulation/modules/CSS.js
+++ src/lib/manipulation/modules/CSS.js
@@ -14,7 +14,7 @@
 const css = function(styles, value) {
 	let returnedStyle, returnStyle;
 	this.each(function() {
-		if (typeof styles === 'object') {
+		if (typeof styles === 'object' && !Array.isArray(styles)) {
 			Object.assign(this.style, styles);
 		} else if (typeof styles === 'string' && (value === undefined || value === null)) {
 			returnedStyle = this.style[styles];
