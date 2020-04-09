543417eb966c2e63b64865d09cca8d49957dc684
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2020-04-09 17:39:58 -0600

    Add prepublish script

---
+++ scripts/prepublish.sh
@@ -0,0 +1,3 @@
+# Run the following command in your terminal to grant permissions to execute the file:
+# chmod +x ./testAll.sh
+npm run lint && npm run docs && npm run build && npm test && npm run clean
